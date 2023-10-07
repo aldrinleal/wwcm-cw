1c99488f9959b67433d777728142cc4ad4dfa4ed
Aldrin Leal <aldrin@leal.eng.br>
2023-10-07 08:52:35 -0500

    First

---
+++ demo.ps1
@@ -0,0 +1,46 @@
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 1
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 2
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 3
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 1
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 2
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 3
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 1
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 2
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 3
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 1
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 2
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 3
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 1
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 2
+Start-Sleep -s 60
+
+aws cloudwatch put-metric-data --namespace wwcode --metric-name Conexiones --value 3
+Start-Sleep -s 60
+
+
